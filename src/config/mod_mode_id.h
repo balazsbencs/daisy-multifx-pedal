251735edd2bb0be6de4ecba9722ae891649cf8d3
Balázs Bencs <balazs.bencs@supercharge.io>
2026-05-22 15:51:31 +0200

    fix: extend ModModeId and add speed ranges for Destroyer and PatternTrem

--- src/config/mod_mode_id.h
+++ src/config/mod_mode_id.h
@@ -4,13 +4,19 @@
 namespace pedal {
 
 enum class ModModeId : uint8_t {
-    Chorus   = 0,
-    Flanger  = 1,
-    Rotary   = 2,
-    Vibe     = 3,
-    Phaser   = 4,
-    VintTrem = 5,
-    COUNT    = 6,
+    Chorus      = 0,
+    Flanger     = 1,
+    Rotary      = 2,
+    Vibe        = 3,
+    Phaser      = 4,
+    VintTrem    = 5,
+    PatternTrem = 6,
+    AutoSwell   = 7,
+    FilterFx    = 8,
+    FormantFx   = 9,
+    Quadrature  = 10,
+    Destroyer   = 11,
+    COUNT       = 12,
 };
 
 } // namespace pedal
