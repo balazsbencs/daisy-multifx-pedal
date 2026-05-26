31c68c0d6603eb5976cbfb9d14cc91ba1f82ab42
Balázs Bencs <balazs.bencs@supercharge.io>
2026-05-26 15:48:53 +0200

    feat: Rust MIDI backend with midir input/output

--- editor/src-tauri/src/main.rs
+++ editor/src-tauri/src/main.rs
@@ -1,6 +1,18 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod commands;
+mod midi;
+mod sysex;
+
+use std::sync::{Arc, Mutex};
+use midi::MidiState;
+
 fn main() {
-    editor_lib::run()
+    let midi_state = Arc::new(Mutex::new(MidiState::new()));
+
+    tauri::Builder::default()
+        .manage(midi_state)
+        .invoke_handler(tauri::generate_handler![])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
